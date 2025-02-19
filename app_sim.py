e76aff4f524cdb7d62a44870eca2e91a93294770
jkammering97 <144028720+jkammering97@users.noreply.github.com>
2025-02-19 15:30:59 +0100

    smaller search terms_final__

--- app_sim.py
+++ app_sim.py
@@ -135,25 +135,6 @@ st.set_page_config(layout="wide", page_title="Neo4j Term Similarity", page_icon=
 
 st.sidebar.header("Select Terms for Similarity Analysis")
 
-st.markdown(
-    """
-    <style>
-    /* Reduce font size of selected terms in the dropdown */
-    div[data-baseweb="tag"] {
-        font-size: 12px !important; /* Adjust size */
-        white-space: nowrap; /* Prevent wrapping */
-    }
-    
-    /* Reduce font size inside the dropdown search box */
-    div[data-baseweb="select"] {
-        font-size: 12px !important; /* Adjust dropdown option size */
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
-
 # Load glossary terms from the embedded dataset
 df_tfnd_glossary_2023 = pd.read_json("data/df_tfnd_glossary_2023_embedded.json", orient="records")
 df_tfnd_glossary_2023["embedding"] = df_tfnd_glossary_2023["embedding"].apply(lambda x: np.array(x, dtype=np.float32))
